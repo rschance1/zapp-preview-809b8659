3114add893f4b390504eeeabebb8224d7de0d9df
Richard Chance <richardchance1980@gmail.com>
2025-10-24 16:51:38 -0500

    Update lib/services/auth_service.dart for preview

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,37 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService {
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  User? get currentUser => _supabase.auth.currentUser;
+  
+  Stream<AuthState> get authStateChanges => _supabase.auth.onAuthStateChange;
+
+  Future<AuthResponse> signUp({
+    required String email,
+    required String password,
+  }) async {
+    return await _supabase.auth.signUp(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<AuthResponse> signIn({
+    required String email,
+    required String password,
+  }) async {
+    return await _supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    await _supabase.auth.signOut();
+  }
+
+  Future<void> resetPassword(String email) async {
+    await _supabase.auth.resetPasswordForEmail(email);
+  }
+}
