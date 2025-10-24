a674320f6c3597415fd6a17544250d82abcb1fb5
Richard Chance <richardchance1980@gmail.com>
2025-10-24 16:51:36 -0500

    Update lib/config/supabase_config.dart for preview

---
+++ lib/config/supabase_config.dart
@@ -0,0 +1,4 @@
+class SupabaseConfig {
+  static const String supabaseUrl = 'https://vwdqqmyiowybmhgrimxv.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3ZHFxbXlpb3d5Ym1oZ3JpbXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk4MDM5OTYsImV4cCI6MjA0NTM3OTk5Nn0.qLxH_vXQYvXGvPvXQYvXGvPvXQYvXGvPvXQYvXGvPvXQ';
+}
