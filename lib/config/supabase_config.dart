4bfd9c4b18567232c6b0be4b0e9a844b99302624
Richard Chance <richardchance1980@gmail.com>
2025-10-24 17:39:13 -0500

    Update lib/config/supabase_config.dart for preview

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,4 +1,5 @@
 class SupabaseConfig {
   static const String supabaseUrl = 'https://vwdqqmyiowybmhgrimxv.supabase.co';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3ZHFxbXlpb3d5Ym1oZ3JpbXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk4MDM5OTYsImV4cCI6MjA0NTM3OTk5Nn0.qLxH_vXQYvXGvPvXQYvXGvPvXQYvXGvPvXQYvXGvPvXQ';
+  
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3ZHFxbXlpb3d5Ym1oZ3JpbXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMzQwMzMsImV4cCI6MjA3NjkxMDAzM30.KcJUvDjLKKjr4DC5mIqKFePCXzZYYtQUewoVKw_lt04';
 }
